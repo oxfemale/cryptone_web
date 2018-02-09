7ecf7aea8b765d5bbcf536ca6d192845fa49086d
lulz <oxfemale@users.noreply.github.com>
2018-02-09 09:50:53 +0300

    Add: Keys exchnage func, global vars
    Add keys exchange functions and Some Global vars for change keys on fly

---
+++ cryptone_web/KeysExchange.cpp
@@ -0,0 +1,297 @@
+#include "stdafx.h"
+#include "globalvars.h"
+#include "UserRegistration.h"
+#include "Randoms.h"
+#include "Container.h"
+#include "AskUsername.h"
+#include "AskPasswords.h"
+#include "http.h"
+#include "PacketFactory.h"
+#include "AddNewClient.h"
+#include "ClientFunctions.h"
+#include "KeysExchange.h"
+
+int UpdateCurrentKeys(unsigned char* strPwd, unsigned char* newAESKey, unsigned char* newAESVector, unsigned char* newClientPrivateKey, unsigned char* newClientPublicKey, unsigned char* newClientCertKey, unsigned char* newServerPublicKey)
+{
+    std::string aesk = "";
+    while (gUpdateKeys == 1)
+    {
+        Sleep(1000);
+    }
+    gUpdateKeys = 1;
+
+    aesk.append(BeginServerPublicKey, strlen(BeginServerPublicKey));
+    aesk.append((char*)newServerPublicKey, strlen((const char*)newServerPublicKey));
+    aesk.append(EndServerPublicKey, strlen(EndServerPublicKey));
+    if (UpdateContainer(strPwd, BeginServerPublicKey, EndServerPublicKey, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+
+    aesk = "";
+    aesk.append(BeginCertificate, strlen(BeginCertificate));
+    aesk.append((char*)newClientCertKey, strlen((const char*)newClientCertKey));
+    aesk.append(EndCertificate, strlen(EndCertificate));
+    if (UpdateContainer(strPwd, BeginCertificate, EndCertificate, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+
+    aesk = "";
+    aesk.append(BeginPublicKey, strlen(BeginPublicKey));
+    aesk.append((char*)newClientPublicKey, strlen((const char*)newClientPublicKey));
+    aesk.append(EndPublicKey, strlen(EndPublicKey));
+    if (UpdateContainer(strPwd, BeginPublicKey, EndPublicKey, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+    
+    aesk = "";
+    aesk.append(BeginPrivateKey, strlen(BeginPrivateKey));
+    aesk.append((char*)newClientPrivateKey, strlen((const char*)newClientPrivateKey));
+    aesk.append(EndPrivateKey, strlen(EndPrivateKey));
+    if (UpdateContainer(strPwd, BeginPrivateKey, EndPrivateKey, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+
+    aesk = "";
+    aesk.append(BeginAESKey, strlen(BeginAESKey));
+    aesk.append((char*)newAESKey, strlen((const char*)newAESKey));
+    aesk.append(EndAESKey, strlen(EndAESKey));
+    if (UpdateContainer(strPwd, BeginAESKey, EndAESKey, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+
+    aesk = "";
+    aesk.append(BeginAESVector, strlen(BeginAESVector));
+    aesk.append((char*)newAESVector, strlen((const char*)newAESVector));
+    aesk.append(EndAESVector, strlen(EndAESVector));
+    if (UpdateContainer(strPwd, BeginAESVector, EndAESVector, (unsigned char*)aesk.c_str()) == 0)
+    {
+        aesk.clear();
+        gUpdateKeys = 0;
+        return 0;
+    }
+    aesk.clear();
+
+    gAESkey = newAESKey;
+    gAESVector = newAESVector;
+    gClientPrivate = newClientPrivateKey;
+    gClientPublic = newClientPublicKey;
+    gClientCert = newClientCertKey;
+    gServerPublic = newServerPublicKey;
+
+
+    gUpdateKeys = 0;
+    return 1;
+}
+
+unsigned char* GegNewKeys(unsigned char** newAESKey2, unsigned char** newAESVector2, unsigned char** newClientPublicKey2, unsigned char** newClientPrivateKey2, unsigned char** newClientCertKey2)
+{
+    int iLen = 0;
+    unsigned char* AllData = NULL;
+    unsigned char* newAESKey = NULL;
+    unsigned char* newAESVector = NULL;
+    unsigned char* newClientPublicKey = NULL;
+    unsigned char* newClientPrivateKey = NULL;
+    unsigned char* newClientCertKey = NULL;
+
+    GenRSAKeysToMem = (GenRSAKeysToMemExp)GetProcAddress(hModuleCRYPT, "GenRSAKeysToMem");
+    if (GenRSAKeysToMem == NULL) return 0;
+
+    //Generate RSA keys and save it to memory
+    if (GenRSAKeysToMem(&newClientPrivateKey, &newClientPublicKey, &newClientCertKey) == false) return NULL;
+
+    newAESKey = gen_random(32);
+    if (newAESKey == NULL)
+    {
+        printf("newAESKey = gen_random error.\r\n");
+        return NULL;
+    }
+
+    newAESVector = gen_random(16);
+    if (newAESVector == NULL)
+    {
+        printf("newAESVector = gen_random error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        return NULL;
+    }
+
+    iLen = strlen((char*)newAESKey) + strlen((char*)newAESVector) + strlen((char*)newClientPublicKey) + 32;
+    AllData = (unsigned char*)VirtualAlloc(NULL, iLen, MEM_COMMIT, PAGE_READWRITE);
+    if (AllData == NULL)
+    {
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        return NULL;
+    }
+    _snprintf((char*)AllData, (iLen - 4), "%s:%s:%s", newAESKey, newAESVector, newClientPublicKey);
+
+    *newAESKey2 = newAESKey;
+    *newAESVector2 = newAESVector;
+    *newClientPublicKey2 = newClientPublicKey;
+    *newClientPrivateKey2 = newClientPrivateKey;
+    *newClientCertKey2 = newClientCertKey;
+
+    return AllData;
+}
+
+/*
+Function:
+The client sends an encrypted request to the server with new AES key/Vector + new Client Public key
+Also, the server returns the new server public key.
+
+vars:
+strPwd - Container password
+
+return:
+int 0 - FAILED
+int 1 if all Ok
+(Also, the server returns a free space on the hard disk of the server.)
+*/
+int ClientServerKeysExchange(unsigned char* strPwd)
+{
+    unsigned char* AllData = NULL;
+    int iLen = 0;
+    unsigned char* NewKeysAllData = NULL;
+    unsigned char* ClientPacket = NULL;
+    unsigned char* newAESKey = NULL;
+    unsigned char* newAESVector = NULL;
+    unsigned char* newClientPublicKey = NULL;
+    unsigned char* newClientPrivateKey = NULL;
+    unsigned char* newClientCertKey = NULL;
+    unsigned char* newServerPublicKey = NULL;
+    char* PackedData = NULL;
+    char* ServerAnswer = NULL;
+    unsigned char* DecryptedData = NULL;
+
+    if (strPwd == NULL)
+    {
+        printf("Error strPwd is null.\r\n");
+        return 0;
+    }
+
+    NewKeysAllData = GegNewKeys( &newAESKey, &newAESVector, &newClientPublicKey, &newClientPrivateKey, &newClientCertKey);
+    if (NewKeysAllData == NULL)
+    {
+        printf("Error: get NewKeysAllData error.\r\n");
+        return NULL;
+    }
+
+    PackedData = PackData(NewKeysAllData);
+    VirtualFree(NewKeysAllData, 0, MEM_RELEASE);
+    if (PackedData == NULL)
+    {
+        printf("Error: get PackedData error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        return NULL;
+    }
+
+    iLen = strlen((char*)gServerPassword) + strlen(PackedData) + 64;
+    AllData = (unsigned char*)VirtualAlloc(NULL, iLen, MEM_COMMIT, PAGE_READWRITE);
+    if (AllData == NULL)
+    {
+        printf("VirtualAlloc error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        return NULL;
+    }
+
+    //clear_data:SubCommand
+    _snprintf((char*)AllData, (iLen - 4), "%s:%s:keys", (char*)gServerPassword, PackedData);
+    VirtualFree(PackedData, 0, MEM_RELEASE);
+
+    ClientPacket = PackClientPacket(AllData, gUseridhash, gAESkey, gAESVector, "job");
+    VirtualFree(AllData, 0, MEM_RELEASE);
+    if (ClientPacket == NULL)
+    {
+        printf("PackClientPacket error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        return NULL;
+    }
+
+
+    //Send Packet to Server
+    ServerAnswer = SendPacketData(gServername, (char*)ClientPacket);
+    printf("ClientPacket:[%s]\r\n", ClientPacket);
+    printf("answer:[%s]\r\n", ServerAnswer);
+    VirtualFree(ClientPacket, 0, MEM_RELEASE);
+    if (ServerAnswer == NULL)
+    {
+        printf("Server return error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        return 0;
+    }
+
+    DecryptedData = DecryptServerPacket(ServerAnswer, gAESkey, gAESVector);
+    VirtualFree(ServerAnswer, 0, MEM_RELEASE);
+    if (DecryptedData == NULL)
+    {
+        printf("Server answer aes256_decrypt error.\r\n");
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        return 0;
+    }
+    if (strstr((char*)DecryptedData, ":KeysExOK"))
+    {
+        iLen = strlen(":KeysExOK");
+        DecryptedData[strlen((char*)DecryptedData) - iLen] = 0;
+        printf("Server Public key: %s\r\n", (char*)DecryptedData);
+        while (UpdateCurrentKeys(strPwd, newAESKey, newAESVector, newClientPrivateKey, newClientPublicKey, newClientCertKey, DecryptedData) == 0)
+        {
+            Sleep(500);
+            printf("Keys updated - FAILED, sleep and trye again.\r\n");
+        }
+        VirtualFree(newAESKey, 0, MEM_RELEASE);
+        VirtualFree(newAESVector, 0, MEM_RELEASE);
+        VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+        VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+        VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+        VirtualFree(DecryptedData, 0, MEM_RELEASE);
+        return 1;
+    }
+    VirtualFree(newAESKey, 0, MEM_RELEASE);
+    VirtualFree(newAESVector, 0, MEM_RELEASE);
+    VirtualFree(newClientPrivateKey, 0, MEM_RELEASE);
+    VirtualFree(newClientPublicKey, 0, MEM_RELEASE);
+    VirtualFree(newClientCertKey, 0, MEM_RELEASE);
+    VirtualFree(DecryptedData, 0, MEM_RELEASE);
+   printf("Keys updated - OK.\r\n");
+
+    return 0;
+}
---
+++ cryptone_web/cryptone_web.cpp
@@ -0,0 +1,155 @@
+/* 
+twitter: @oxfemale
+emails: root@eas7.ru or root@kitsune.online
+telegram: https://t.me/BelousovaAlisa
+*/
+#include "stdafx.h"
+#include "globalvars.h"
+#include "cryptone_web.h"
+#include "PacketFactory.h"
+#include "SystemInfo.h"
+#include "Container.h"
+#include "AskPasswords.h"
+#include "AskUsername.h"
+#include "http.h"
+#include "Randoms.h"
+#include "UserRegistration.h"
+#include "AddNewClient.h"
+#include "ClientFunctions.h"
+#include "ReadCfgFiles.h"
+#include "KeysExchange.h"
+#pragma comment (lib, "Wininet.lib")
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char* cryptone = "cryptone.dll";
+	hModuleCRYPT = NULL;
+	unsigned char* strPwd = NULL;
+	FILE* pFile = NULL;
+    char* Servername = NULL;
+
+   
+
+    hModuleCRYPT = LoadLibraryA(cryptone);
+    if (hModuleCRYPT == NULL)
+    {
+        printf("Error[%d] load [%s] dll\r\n", GetLastError(), cryptone);
+        return 0;
+    }
+
+    if (SetDefaultAESVector() == 0)
+    {
+        printf("Error set default aes vector.\r\n");
+        return 0;
+    }
+
+    if (ServersList() == 0)
+    {
+        printf("Error set servername.\r\n");
+        return 0;
+    }
+    Servername = gServername;
+
+    if( doPingServer(Servername) == 1 )
+	{
+		printf("Server is Alive.\r\n");
+		bestType = SelectBestHttpTraffic(Servername);
+	}else
+	{
+		printf("Server is Down.\r\n");
+		return 0;
+	}
+
+
+	pFile = fopen("file", "rb");
+	if(pFile != NULL)
+	{
+		fclose(pFile);
+		FILE* pasFile = NULL;
+		pasFile = fopen("pass", "rb");
+		if(pasFile == NULL)
+		{
+			strPwd = (unsigned char*)AskContainerPassword( );
+			if( strPwd == NULL ) return 0;
+		}else{
+			strPwd =  (unsigned char*)VirtualAlloc( NULL, 32, MEM_COMMIT, PAGE_READWRITE );
+			if( strPwd == NULL )
+			{
+				fclose(pFile);
+				fclose(pasFile);
+				printf("Error VirtualAlloc for password buffer.\r\n");
+				return 0;
+			}
+			memset( strPwd, '-', 32 );
+			fread( strPwd, 1, 32, pasFile);
+			fclose(pasFile);
+		}
+
+		//printf( "\r\nPassword is: \r\n%s\r\n", strPwd );
+		if( IsContainer() == 1 )
+		{
+			if( TestCfgVars( strPwd ) == 0 )
+			{
+				printf("Error test Container vars.\r\n");
+				remove( "file" );
+				printf("Container keys is deleted, please, goto login/register again.\r\n");
+				return 0;
+			}
+		
+			if ( isRegUser( strPwd ) == 0 )
+			{
+				printf("Not reggged user.\r\n");
+				remove( "file" );
+				printf("Container keys is deleted, please, goto login/register again.\r\n");
+				return 0;
+			}else{
+				printf("Reggged user.\r\n");
+				
+				if ( ClientPingServer( Servername, strPwd ) == 0 )
+				{
+					printf("Server ping ERROR.\r\n");
+					return 0;
+				}
+				printf("Server ping OK.\r\n");
+
+                if (SetKeysMem(strPwd) == 0)
+                {
+                    printf("Read Global keys error.\r\n");
+                    return 0;
+                }
+                ClientServerKeysExchange(strPwd);
+                if (ClientPingServer(Servername, strPwd) == 0)
+                {
+                    printf("Server ping with new keys ERROR.\r\n");
+                    return 0;
+                }
+                printf("Server ping with new keys OK.\r\n");
+
+				return 1;
+			}
+		}else{
+			//remove( "file" );
+			printf("Error: Container password is wrong or container file is bad.\r\n");
+			return 0;
+		}
+	}else
+	{
+		if ( NewUserRegistration( Servername ) == 0 )
+		{
+			printf("Registration error.\r\n");
+            remove("file");
+			return 0;
+		}else
+		{
+			printf("Registration Ok\r\n");
+			return 1;
+		}
+		VirtualFree( strPwd, 0, MEM_RELEASE );
+	}
+
+
+	return 0;
+}
+
+
